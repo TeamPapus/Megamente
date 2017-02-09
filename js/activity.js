0f523709cadb595636451be4957ce91b77793604
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-02-09 16:55:12 -0600

    arrastre logrado

--- js/activity.js
+++ js/activity.js
@@ -4,6 +4,27 @@ define(function (require) {
     var jquery = require("jquery");
     var interact = require("interact");
 
+ var moveItem = function(event) {
+        // Current element
+        var target = event.target;
+        // Get axis values + movement change
+        if (!target.hasAttribute('data-x')) {
+            x0 = $(target).position().top;
+            y0 = $(target).position().left;
+        }
+
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        // Transform element
+        target.style.top = x0 + 'px';
+        target.style.left = y0 + 'px';
+        target.style.webkitTransform =
+        target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+        // Update element attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    };
 
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
@@ -17,9 +38,20 @@ define(function (require) {
         	$('#deduccion').toggle();
 
         });
+        
+        $('#btn2').on('click', function(){
+        	$('#menu').toggle();
+        	$('#quiensoy').toggle();
 
+        });
 
+        $('backgroud-personajes').css('backgroud');
+        var items = interact(".draggable");
 
+                items.draggable({
+                    initial: true,
+                    onmove: moveItem,
+                });
 
     });
 
