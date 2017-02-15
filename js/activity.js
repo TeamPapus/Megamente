f4885a6c4d331370ed09ac3df171384c0b27697e
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-02-14 19:11:14 -0600

    navegacion finalizada

--- js/activity.js
+++ js/activity.js
@@ -5,14 +5,15 @@ define(function (require) {
     var interact = require("interact");
     
     var mi_imagen = new Image();
-    var imagen = '../Megamente/img/deduccion/deduccion/ejercicio1/zapato-amarillo.png';
+    var imagen = '../Megamente/img/deduccion/objetos/zapato-amarillo.png';
     function load_image() {
         mi_imagen.src = imagen;
         console.log("Tamaño de imagen: ");
         console.log(mi_imagen.height+'x'+mi_imagen.width);
     }
     
- var moveItem = function(event) {
+    // funcion de movimiento para objetos html
+    var moveItem = function(event) {
         // Current element
         var target = event.target;
         // Get axis values + movement change
@@ -20,59 +21,39 @@ define(function (require) {
             x0 = $(target).position().top;
             y0 = $(target).position().left;
         }
-
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
         // Transform element
         target.style.top = x0 + 'px';
         target.style.left = y0 + 'px';
-        target.style.webkitTransform =
-        target.style.transform =
-            'translate(' + x + 'px, ' + y + 'px)';
+        target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
         // Update element attributes
         target.setAttribute('data-x', x);
         target.setAttribute('data-y', y);
     };
-
-
     
     // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
-      
 
         // Initialize the activity.
         activity.setup();
-        
 
         load_image();
 
-        $('#btn').on('click', function(){
+        $('#btn-deduccion').on('click', function(){
         	$('#menu').toggle();
         	$('#deduccion').toggle();
-             load_image();
-
+            load_image();
         });
         
-        $('#btn2').on('click', function(){
+        $('#btn-quiensoy').on('click', function(){
         	$('#menu').toggle();
         	$('#quiensoy').toggle();
-
         });
         
-          $('#inicio').on('click', function(){
-        	$('#deduccion').toggle();
+        $('.inicio').on('click', function(){
+            $(this).parents('.padre').toggle();
         	$('#menu').toggle();
-
         });
-
-        $('backgroud-personajes').css('backgroud');
-        var items = interact(".draggable");
-
-                items.draggable({
-                    initial: true,
-                    onmove: moveItem,
-                });
-
     });
-
 });
