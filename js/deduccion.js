d582bb36d5715addd14d0ccfe66ab75b33e2729a
Linda Isabel Martinez Castro <linkl3@hotmail.com>
2017-02-16 16:59:06 -0600

    matriz

--- js/deduccion.js
+++ js/deduccion.js
@@ -1,32 +1,80 @@
 define(function (require) {
     return [
-    {
-    	'img' : [
-    		'../img/deduccion/objetos/zapato-azul.svg',
-    		'../img/deduccion/objetos/zapato-amarillo.svg',
-    		'../img/deduccion/objetos/zapato-anaranjado.svg',
-			'../img/deduccion/objetos/zapato-rojo.svg',
-			'../img/deduccion/objetos/zapato-verde.svg'
+        {
+            'img' : [
+                '../img/deduccion/personajes/juan.svg',
+                '../img/deduccion/personajes/beatriz.svg',
+                '../img/deduccion/personajes/alonso.svg',
+                '../img/deduccion/personajes/irene.svg',
+                '../img/deduccion/personajes/ana.svg'
 		],
-		'pista' : [
-			'1). El zapato verde es menos cómodo que el zapato gris.',
-			'2). El zapato azul es más cómodo que el zapato naranja y menos que el zapato amarillo.',
-			'3). El zapato gris es menos cómodo que el zapato naranja.'
+            'pista' : [
+                '¿Qué personaje es mayor?',
+                '1). Irene es mayor que Ana y menor que Alonso.',
+                '2). Beatriz es mayor que Alonso.',
+                '3). Juan es mayor que Beatriz.'
 		]
-	},	
-	{
-		'img' : [
-			'../img/deduccion/personajes/ana.svg',
-			'../img/deduccion/personajes/antonio.svg',
-			'../img/deduccion/personajes/cristina.svg',
-			'../img/deduccion/personajes/david.svg',
-			'../img/deduccion/personajes/gabriela.svg'
+        },
+        {
+            'img' : [
+                '../img/deduccion/objetos/avion-verde.svg',
+                '../img/deduccion/objetos/avion-rojo.svg',
+                '../img/deduccion/objetos/avion-naranja.svg',
+                '../img/deduccion/objetos/avion-amarillo.svg',
+                '../img/deduccion/objetos/avion-azul.svg'
 		],
-		'pista' : [
-			'1). Irene es mayor que Ana y menor que Alonso.',
-	 		'2). Beatriz es mayor que Alonso.',
-	 		'3). Juan es mayor que Beatriz.'
-		]
-	}
-	];
+            'pista' : [
+                '¿Qué avión vuela más alto?',
+                '1). El avión amarillo vuela más que el avión azul y menos que el avión naranja.',
+                '2). El avión rojo vuela más que el avión verde.',
+                '3). El avión naranja vuela menos que el avión verde.'
+            ]
+        },
+        
+        {
+            'img' : [
+                '../img/deduccion/alimentos/naranja.svg',
+                '../img/deduccion/alimentos/fresa.svg',
+                '../img/deduccion/alimentos/sandia.svg',
+                '../img/deduccion/alimentos/manzana.svg',
+                '../img/deduccion/alimentos/pera.svg'
+            ],
+            'pista' : [
+                '¿Qué fruta es menos jugosa?',
+                '1). La pera es más jugosa que la manzana.',
+                '2). La sandía es menos jugosa que la manzana.',
+                '3). La fresa es más jugosa que la naranja y menos que la sandía.'
+            ]
+        }, 
+        
+        {
+            'img' : [
+                '../img/deduccion/objetos/camisa-verde.svg',
+                '../img/deduccion/objetos/camisa-rosa.svg',
+                '../img/deduccion/objetos/camisa-naranja.svg',
+                '../img/deduccion/objetos/camisa-amarilla.svg',
+                '../img/deduccion/objetos/camisa-gris.svg'
+            ],
+            'pista' : [
+                '¿Qué camisa es más cómoda?',
+                '1). La camisa amarilla es más cómoda que la camisa gris y menos que la camisa naranja.',
+                '2). La camisa rosa es más cómoda que la camisa naranja.',
+                '3). La camisa verde es más cómoda que la camisa rosa.'
+            ]
+        },
+        {
+            'img' : [
+                '../img/deduccion/alimentos/ciruela.svg',
+                '../img/deduccion/alimentos/piña.svg',
+                '../img/deduccion/alimentos/guayaba.svg',
+                '../img/deduccion/alimentos/uva.svg',
+                '../img/deduccion/alimentos/pitahaya.svg'
+            ],
+            'pista' : [
+                '¿Qué fruta es menos jugosa?',
+                '1). La piña es menos jugosa que la guayaba y más que la ciruela.',
+                '2). La uva es más jugosa que la guayaba y menos que la pitaya.'
+            ]
+        }  
+    ];
 });
