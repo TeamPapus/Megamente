15e1dfc73ab2e104331883cef5b384ebc02e4bb8
Lester Mendoza <lestermt94@gmail.com>
2017-02-22 17:26:58 -0600

    incluscion de seleccion de elementon nivel quen soy

--- js/deduccion.js
+++ js/deduccion.js
@@ -2,11 +2,11 @@ define(function (require) {
     return [
         {
             'img' : [
-                '../img/deduccion/personajes/juan.svg',
-                '../img/deduccion/personajes/beatriz.svg',
-                '../img/deduccion/personajes/alonso.svg',
-                '../img/deduccion/personajes/irene.svg',
-                '../img/deduccion/personajes/ana.svg'
+                '../Megamente/img/deduccion/personajes/juan.svg',
+                '../Megamente/img/deduccion/personajes/beatriz.svg',
+                '../Megamente/img/deduccion/personajes/alonso.svg',
+                '../Megamente/img/deduccion/personajes/irene.svg',
+                '../Megamente/img/deduccion/personajes/ana.svg'
 		],
             'pista' : [
                 '¿Qué personaje es mayor? 1). Irene es mayor que Ana y menor que Alonso. 2). Beatriz es mayor que Alonso. 3). Juan es mayor que Beatriz.'
@@ -14,37 +14,37 @@ define(function (require) {
         },
         {
             'img' : [
-                '../img/deduccion/objetos/avion-verde.svg',
-                '../img/deduccion/objetos/avion-rojo.svg',
-                '../img/deduccion/objetos/avion-naranja.svg',
-                '../img/deduccion/objetos/avion-amarillo.svg',
-                '../img/deduccion/objetos/avion-azul.svg'
+                '../Megamente/img/deduccion/objetos/avion-verde.svg',
+                '../Megamente/img/deduccion/objetos/avion-rojo.svg',
+                '../Megamente/img/deduccion/objetos/avion-naranja.svg',
+                '../Megamente/img/deduccion/objetos/avion-amarillo.svg',
+                '../Megamente/img/deduccion/objetos/avion-azul.svg'
 		],
             'pista' : [
                 '¿Qué avión vuela más alto? 1). El avión amarillo vuela más que el avión azul y menos que el avión naranja. 2). El avión rojo vuela más que el avión verde. 3). El avión naranja vuela menos que el avión verde.'
             ]
         },
-        
+
         {
             'img' : [
-                '../img/deduccion/alimentos/naranja.svg',
-                '../img/deduccion/alimentos/fresa.svg',
-                '../img/deduccion/alimentos/sandia.svg',
-                '../img/deduccion/alimentos/manzana.svg',
-                '../img/deduccion/alimentos/pera.svg'
+                '../Megamente/img/deduccion/alimentos/naranja.svg',
+                '../Megamente/img/deduccion/alimentos/fresa.svg',
+                '../Megamente/img/deduccion/alimentos/sandia.svg',
+                '../Megamente/img/deduccion/alimentos/manzana.svg',
+                '../Megamente/img/deduccion/alimentos/pera.svg'
             ],
             'pista' : [
                 '¿Qué fruta es menos jugosa?. 1). La pera es más jugosa que la manzana. 2). La sandía es menos jugosa que la manzana. 3). La fresa es más jugosa que la naranja y menos que la sandía.'
             ]
-        }, 
-        
+        },
+
         {
             'img' : [
-                '../img/deduccion/objetos/camisa-verde.svg',
-                '../img/deduccion/objetos/camisa-rosa.svg',
-                '../img/deduccion/objetos/camisa-naranja.svg',
-                '../img/deduccion/objetos/camisa-amarilla.svg',
-                '../img/deduccion/objetos/camisa-gris.svg'
+                '../Megamente/img/deduccion/objetos/camisa-verde.svg',
+                '../Megamente/img/deduccion/objetos/camisa-rosa.svg',
+                '../Megamente/img/deduccion/objetos/camisa-naranja.svg',
+                '../Megamente/img/deduccion/objetos/camisa-amarilla.svg',
+                '../Megamente/img/deduccion/objetos/camisa-gris.svg'
             ],
             'pista' : [
                 '¿Qué camisa es más cómoda? 1). La camisa amarilla es más cómoda que la camisa gris y menos que la camisa naranja. 2). La camisa rosa es más cómoda que la camisa naranja. 3). La camisa verde es más cómoda que la camisa rosa.'
@@ -52,15 +52,15 @@ define(function (require) {
         },
         {
             'img' : [
-                '../img/deduccion/alimentos/ciruela.svg',
-                '../img/deduccion/alimentos/piña.svg',
-                '../img/deduccion/alimentos/guayaba.svg',
-                '../img/deduccion/alimentos/uva.svg',
-                '../img/deduccion/alimentos/pitahaya.svg'
+                '../Megamente/img/deduccion/alimentos/ciruela.svg',
+                '../Megamente/img/deduccion/alimentos/piña.svg',
+                '../Megamente/img/deduccion/alimentos/guayaba.svg',
+                '../Megamente/img/deduccion/alimentos/uva.svg',
+                '../Megamente/img/deduccion/alimentos/pitahaya.svg'
             ],
             'pista' : [
                 '¿Qué fruta es menos jugosa?. 1). La piña es menos jugosa que la guayaba y más que la ciruela. 2). La uva es más jugosa que la guayaba y menos que la pitaya.'
             ]
-        }  
+        }
     ];
 });
--- js/quiensoy.js
+++ js/quiensoy.js
@@ -2,12 +2,12 @@ define(function (require) {
     return [
     {
     	'img' : [
-    		'../img/quien-soy/ejercicio1/maria.svg',
-    		'../img/quien-soy/ejercicio1/ines.svg',
-    		'../img/quien-soy/ejercicio1/luisa.svg',
-			'../img/quien-soy/ejercicio1/paula.svg',
-			'../img/quien-soy/ejercicio1/ana.svg',
-            '../img/quien-soy/ejercicio1/raquel.svg'
+    		'../Megamente/img/quien-soy/ejercicio1/maria.svg',
+    		'../Megamente/img/quien-soy/ejercicio1/ines.svg',
+    		'../Megamente/img/quien-soy/ejercicio1/luisa.svg',
+			'../Megamente/img/quien-soy/ejercicio1/paula.svg',
+			'../Megamente/img/quien-soy/ejercicio1/ana.svg',
+            '../Megamente/img/quien-soy/ejercicio1/raquel.svg'
 		],
 		'pista' : [
 			'1). Inés usa gafas.',
@@ -18,22 +18,30 @@ define(function (require) {
             '6). Raquel, Paula, María, Ana llevan teléfono.'
 		],
         'obj' : [
-            '../img/deduccion/objetos/lentes.png',
-            '../img/deduccion/objetos/reloj.png',
-            '../img/deduccion/objetos/collar.png',
-            '../img/deduccion/objetos/sombrero.png',
-            '../img/deduccion/objetos/mochila2.png',
-            '../img/deduccion/objetos/telefono.png'
+            ['../Megamente/img/deduccion/objetos/lentes.png', 'lentes'],
+            ['../Megamente/img/deduccion/objetos/reloj.png', 'reloj'],
+            ['../Megamente/img/deduccion/objetos/collar.png', 'collar'],
+            ['../Megamente/img/deduccion/objetos/sombrero.png', 'sombrero'],
+            ['../Megamente/img/deduccion/objetos/mochila2.png', 'mochila'],
+            ['../Megamente/img/deduccion/objetos/telefono.png', 'telefono']
+        ],
+        'nombres' : [
+            'Inés',
+			'Luisa',
+			'Paula',
+            'María',
+            'Raquel',
+            'Ana'
         ]
-	},	
+	},
 	{
 		'img' : [
-			'../img/quien-soy/ejercicio2/ana.svg',
-			'../img/quien-soy/ejercicio2/david.svg',
-			'../img/quien-soy/ejercicio2/juan.svg',
-			'../img/quien-soy/ejercicio2/jose.svg',
-			'../img/quien-soy/ejercicio2/antonio.svg',
-            '../img/quien-soy/ejercicio2/luisa.svg'
+			'../Megamente/img/quien-soy/ejercicio2/ana.svg',
+			'../Megamente/img/quien-soy/ejercicio2/david.svg',
+			'../Megamente/img/quien-soy/ejercicio2/juan.svg',
+			'../Megamente/img/quien-soy/ejercicio2/jose.svg',
+			'../Megamente/img/quien-soy/ejercicio2/antonio.svg',
+            '../Megamente/img/quien-soy/ejercicio2/luisa.svg'
 		],
 		'pista' : [
 			'1). Ana, David y Juan cantan.',
@@ -43,25 +51,33 @@ define(function (require) {
             '5). David, Luisa y Antonio tocan el piano.',
             '6). Luisa, Juan, José y Antonio juegan videojuegos.'
 		],
-        
+
         'obj' : [
-            '../img/deduccion/objetos/microfono.png',
-            '../img/deduccion/objetos/camara.png',
-            '../img/deduccion/objetos/guitarra.png',
-            '../img/deduccion/objetos/bicicleta.png',
-            '../img/deduccion/objetos/piano.png',
-            '../img/deduccion/objetos/videojuego.png'
+            ['../Megamente/img/deduccion/objetos/microfono.png', 'microfono'],
+            ['../Megamente/img/deduccion/objetos/camara.png', 'camara'],
+            ['../Megamente/img/deduccion/objetos/guitarra.png', 'guitarra'],
+            ['../Megamente/img/deduccion/objetos/bicicleta.png', 'bicicleta'],
+            ['../Megamente/img/deduccion/objetos/piano.png', 'piano'],
+            ['../Megamente/img/deduccion/objetos/videojuego.png', 'video juego']
+        ],
+        'nombres' : [
+            'Ana',
+			'David',
+			'Juan',
+            'José',
+            'Luisa',
+            'Antonio'
         ]
 	},
-        {
-            'img' : [
-                '../img/quien-soy/ejercicio3/gabriela.svg',
-                '../img/quien-soy/ejercicio3/maria.svg',
-                '../img/quien-soy/ejercicio3/ana.svg',
-                '../img/quien-soy/ejercicio3/javier.svg',
-                '../img/quien-soy/ejercicio3/jose.svg',
-                '../img/quien-soy/ejercicio3/paula.svg'
-		],
+    {
+        'img' : [
+            '../Megamente/img/quien-soy/ejercicio3/gabriela.svg',
+            '../Megamente/img/quien-soy/ejercicio3/maria.svg',
+            '../Megamente/img/quien-soy/ejercicio3/ana.svg',
+            '../Megamente/img/quien-soy/ejercicio3/javier.svg',
+            '../Megamente/img/quien-soy/ejercicio3/jose.svg',
+            '../Megamente/img/quien-soy/ejercicio3/paula.svg'
+    	],
             'pista' : [
                 '1). Gabriela, María, José y Javier cantan.',
                 '2). Gabriela toma fotografías.',
@@ -69,15 +85,23 @@ define(function (require) {
                 '4). José monta en bicicleta.',
                 '5). María, Ana y Javier tocan el piano.',
                 '6). Paula juega videojuegos.'
-		],
+    	],
             'obj' : [
-                '../img/deduccion/objetos/microfono.png',
-                '../img/deduccion/objetos/camara.png',
-                '../img/deduccion/objetos/guitarra.png',
-                '../img/deduccion/objetos/bicicleta.png',
-                '../img/deduccion/objetos/piano.png',
-                '../img/deduccion/objetos/videojuego.png'
-        ]    
-	}
-	];
+                ['../Megamente/img/deduccion/objetos/microfono.png', 'microfono'],
+                ['../Megamente/img/deduccion/objetos/camara.png', 'camara'],
+                ['../Megamente/img/deduccion/objetos/guitarra.png', 'guitarra'],
+                ['../Megamente/img/deduccion/objetos/bicicleta.png', 'bicicleta'],
+                ['../Megamente/img/deduccion/objetos/piano.png', 'piano'],
+                ['../Megamente/img/deduccion/objetos/videojuego.png', 'video juego']
+        ],
+        'nombres' : [
+            'Gabriela',
+    		'María',
+    		'José',
+            'Ana',
+            'Javier',
+            'Paula'
+        ]
+    }
+    ];
 });
